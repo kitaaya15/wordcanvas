a43958cb3f97268ef271a0bcba343579e513a749
kitaaya15 <maaanyan1215@gmail.com>
2024-04-25 23:51:37 +0900

    詳細ページ実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "words#index"
-  resources :words, only: [:new, :create]
+  resources :words, only: [:new, :create, :show]
   resources :users
 end
