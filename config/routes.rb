81ef6355eda8e266720bcca6ac719a30db42124a
kitaaya15 <maaanyan1215@gmail.com>
2024-06-18 12:20:33 +0900

    ユーザー詳細一覧実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "tweets#index"
   resources :words, only: [:new, :create, :show]
-  resources :users
+  resources :users, only: [:show, :edit, :update]
   resources :tweets do
     collection do
       get 'search'
